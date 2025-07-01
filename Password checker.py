1e2795c6034eacb97e92c69f65204575158a7e6f
roopesh907 <roopeshmallem@gmail.com>
2025-07-01 15:25:29 +0530

    Add login system project using if-else

---
+++ Password checker.py
@@ -0,0 +1,12 @@
+# Simple login system using if-else in Python
+
+username = 'roopesh'  
+password = 'Python123'  
+
+entered_username = 'roopesh'  
+entered_password = 'Python123'  
+
+if entered_username == username and entered_password == password:  
+    print('Access Granted')  
+else:  
+    print('Access Denied')
